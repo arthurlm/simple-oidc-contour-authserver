1a3ea6af4ce54b4929e0eed3ec0066ccd7f9361e
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2021-03-17 18:10:05 +0100

    Add TLS support for server

--- src/main.rs
+++ src/main.rs
@@ -54,24 +54,55 @@ mod auth;
 mod checkers;
 mod token_validation;
 
+use clap::Clap;
+use futures::try_join;
 use std::sync::Arc;
-use tonic::transport::Server;
+use tonic::transport::{Identity, Server, ServerTlsConfig};
 
 use crate::envoy::service::auth::v2::authorization_server::AuthorizationServer as AuthorizationServerV2;
 use crate::envoy::service::auth::v3::authorization_server::AuthorizationServer as AuthorizationServerV3;
+
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Kevin K. <kbknapp@gmail.com>")]
+struct Opts {
+    /// Addr to bind on
+    #[clap(short, long, default_value = "0.0.0.0:50051")]
+    addr: String,
+
+    /// TLS key file to read
+    #[clap(long, default_value = "tls.key")]
+    tls_key: String,
+
+    /// TLS cert file to read
+    #[clap(long, default_value = "tls.crt")]
+    tls_cert: String,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    let auth_service = Arc::new(auth::AuthenticationService::from_env().unwrap());
+    // Parse CLI
+    let opts = Opts::parse();
+    let addr = opts.addr.parse()?;
 
-    let addr = "0.0.0.0:50051".parse()?;
+    // Read TLS data
+    let (cert, key) = try_join!(
+        tokio::fs::read(opts.tls_cert),
+        tokio::fs::read(opts.tls_key)
+    )?;
+
+    let identity = Identity::from_pem(cert, key);
+
+    // Create auth services
+    let auth_service = Arc::new(auth::AuthenticationService::from_env()?);
+
     let auth_v2 = checkers::v2::AuthorizationV2::new(auth_service.clone());
     let auth_v3 = checkers::v3::AuthorizationV3::new(auth_service);
 
     log::info!("gRPC server will listen at: {:?}", addr);
     Server::builder()
+        .tls_config(ServerTlsConfig::new().identity(identity))?
         .add_service(AuthorizationServerV2::new(auth_v2))
         .add_service(AuthorizationServerV3::new(auth_v3))
         .serve(addr)
